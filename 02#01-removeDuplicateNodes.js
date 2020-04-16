f92545dd909cf1039bbb46694e90b9b6cb26e5fc
cookiy <cookiy8511@gmail.com>
2020-04-16 15:26:43 +0800

    增加2道新题

---
+++ 02#01-removeDuplicateNodes.js
@@ -0,0 +1,52 @@
+/**
+编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+
+示例1:
+
+ 输入：[1, 2, 3, 3, 2, 1]
+ 输出：[1, 2, 3]
+示例2:
+
+ 输入：[1, 1, 1, 1, 2]
+ 输出：[1, 2]
+提示：
+
+链表长度在[0, 20000]范围内。
+链表元素在[0, 20000]范围内。
+进阶：
+
+如果不得使用临时缓冲区，该怎么解决？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicate-node-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeDuplicateNodes = function(head) {
+    let arr = [];
+    let prev = null;
+    let current = head;
+    while(current != null){
+        if(arr[current.val] > 0){
+            prev.next = current.next;
+            current = prev.next;
+        } else {
+            arr[current.val] = 1;
+            prev= current;
+            current = current.next;
+        }
+    }
+    return head;
+};
